02226248535136b2bbe18539adc4dd313e06ad5f
Luke S Thompson <73861168+lsthompson@users.noreply.github.com>
2022-03-12 15:39:06 +1100

    House-keeping, case brevity, tabbing/help/etc

--- fixperms.sh
+++ fixperms.sh
@@ -8,20 +8,19 @@
 # Set verbose to null
 verbose=""
 
-
-#Print the help text
+# Print the help text
 helptext () {
     tput bold
     tput setaf 2
-    echo "Fix perms script help:"
+    echo "Fix Permissions (fixperms) Script help:"
     echo "Sets file/directory permissions to match suPHP and FastCGI schemes"
     echo "USAGE: fixperms [options] -a account_name"
     echo "-------"
     echo "Options:"
-    echo "-h or --help: print this screen and exit"
-    echo "-v: verbose output"
-    echo "-all: run on all cPanel accounts"
-    echo "--account or -a: specify a cPanel account"
+    echo "-h or --help: Print this screen and exit"
+    echo "--account or -a: Specify a cPanel account"
+    echo "-all: Run on all cPanel accounts"
+    echo "-v: Verbose output"
     tput sgr0
     exit 0
 }
@@ -29,134 +28,127 @@ helptext () {
 # Main workhorse, fix perms per account passed to it
 fixperms () {
 
-  #Get account from what is passed to the function
-  account=$1
-
-  #Check account against cPanel users file
-  if ! grep $account /var/cpanel/users/*
-  then
-    tput bold
-    tput setaf 1
-    echo "Invalid cPanel account"
-    tput sgr0
-    exit 0
-  fi
-
-  #Make sure account isn't blank
-  if [ -z $account ]
-  then
-    tput bold
-    tput setaf 1
-    echo "Need an account name!"
-    tput sgr0
-    helptext
-  #Else, start doing work
-  else
-
-    #Get the account's homedir
-    HOMEDIR=$(egrep "^${account}:" /etc/passwd | cut -d: -f6)
-
-    tput bold
-    tput setaf 4
-    echo "Fixing perms for $account:"
-    tput setaf 3
-    echo "------------------------"
-    tput setaf 4
-    echo "Fixing website files...."
-    tput sgr0
+    # Get account from what is passed to the function
+    account=$1
     
-    #Fix individual files in public_html
-    find $HOMEDIR/public_html -type d -exec chmod $verbose 755 {} \;
-    find $HOMEDIR/public_html -type f | xargs -d$'\n' -r chmod $verbose 644
-    find $HOMEDIR/public_html -name '*.cgi' -o -name '*.pl' | xargs -r chmod $verbose 755
-    # Hidden files support: https://serverfault.com/a/156481
-    # fix hidden files and folders like .well-known/ with root or other user perms
-    chown $verbose -R $account:$account $HOMEDIR/public_html/.[^.]*
-    find $HOMEDIR/* -name .htaccess -exec chown $verbose $account.$account {} \;
+    # Check account against cPanel user files
+    if ! grep $account /var/cpanel/users/*
+    then
+        tput bold
+        tput setaf 1
+        echo "Invalid cPanel account!"
+        tput sgr0
+    exit 0
+    fi
+    
+    # Make sure account isn't blank
+    if [ -z $account ]
+    then
+        tput bold
+        tput setaf 1
+        echo "Need a cPanel account!"
+        tput sgr0
+        helptext
+    # Else, start doing work
+    else
 
-    tput bold
-    tput setaf 4
-    echo "Fixing public_html...."
-    tput sgr0
-    #Fix perms of public_html itself
-    chown $verbose $account:nobody $HOMEDIR/public_html
-    chmod $verbose 750 $HOMEDIR/public_html
+        # Get the account's homedir
+        HOMEDIR=$(egrep "^${account}:" /etc/passwd | cut -d: -f6)
 
-    #Fix subdomains that lie outside of public_html
-    tput setaf 3
-    tput bold
-    echo "------------------------"
-    tput setaf 4
-    echo "Fixing any domains with a document root outside of public_html...."
-    for SUBDOMAIN in $(grep -i documentroot /var/cpanel/userdata/$account/* | grep -v '.cache\|_SSL' | awk '{print $2}' | grep -v public_html)
-    do
-      tput bold
-      tput setaf 4
-      echo "Fixing sub/addon domain document root $SUBDOMAIN...."
-      tput sgr0
-      find $SUBDOMAIN -type d -exec chmod $verbose 755 {} \;
-      find $SUBDOMAIN -type f | xargs -d$'\n' -r chmod $verbose 644
-      find $SUBDOMAIN -name '*.cgi' -o -name '*.pl' | xargs -r chmod $verbose 755
-      chown $verbose -R $account:$account $SUBDOMAIN
-      chmod $verbose 755 $SUBDOMAIN
-      find $SUBDOMAIN -name .htaccess -exec chown $verbose $account.$account {} \;
-    done
+        tput bold
+        tput setaf 4
+        echo "(fixperms) for: $account"
+        tput setaf 3
+        echo "--------------------------"
+        tput setaf 4
+        echo "Fixing website files..."
+        tput sgr0
+    
+        # Fix individual files in public_html
+        find $HOMEDIR/public_html -type d -exec chmod $verbose 755 {} \;
+        find $HOMEDIR/public_html -type f | xargs -d$'\n' -r chmod $verbose 644
+        find $HOMEDIR/public_html -name '*.cgi' -o -name '*.pl' | xargs -r chmod $verbose 755
+        # Hidden files support - ref: https://serverfault.com/a/156481
+        # Fix hidden files and folders like .well-known/ with root or other user perms
+        chown $verbose -R $account:$account $HOMEDIR/public_html/.[^.]*
+        find $HOMEDIR/* -name .htaccess -exec chown $verbose $account.$account {} \;
 
-  #Finished
-    tput bold
-    tput setaf 3
-    echo "Finished!"
-    echo "------------------------"
-    printf "\n\n"
-    tput sgr0
-  fi
+        tput bold
+        tput setaf 4
+        echo "Fixing public_html itself..."
+        tput sgr0
+        # Fix perms of public_html itself
+        chown $verbose $account:nobody $HOMEDIR/public_html
+        chmod $verbose 750 $HOMEDIR/public_html
 
-  return 0
+        # Fix subdomains that lie outside of public_html
+        tput setaf 3
+        tput bold
+        echo "--------------------------"
+        tput setaf 4
+        echo "Fixing any domains with a docroot outside public_html..."
+        for SUBDOMAIN in $(grep -i documentroot /var/cpanel/userdata/$account/* | grep -v '.cache\|_SSL' | awk '{print $2}' | grep -v public_html)
+        do
+            tput bold
+            tput setaf 4
+            echo "Fixing sub/addon domain docroot for $SUBDOMAIN..."
+            tput sgr0
+            find $SUBDOMAIN -type d -exec chmod $verbose 755 {} \;
+            find $SUBDOMAIN -type f | xargs -d$'\n' -r chmod $verbose 644
+            find $SUBDOMAIN -name '*.cgi' -o -name '*.pl' | xargs -r chmod $verbose 755
+            chown $verbose -R $account:$account $SUBDOMAIN
+            chmod $verbose 755 $SUBDOMAIN
+            find $SUBDOMAIN -name .htaccess -exec chown $verbose $account.$account {} \;
+        done
+
+        # Finished
+        tput bold
+        tput setaf 3
+        echo "Finished! (User: $account)"
+        echo "--------------------------"
+        printf "\n\n"
+        tput sgr0
+    fi
+
+    return 0
 }
 
-#Parses all users through cPanel's users file
+# Parses all users via cPanel's users/domains file
 all () {
     for user in $(cut -d: -f1 /etc/domainusers)
     do
-  fixperms $user
+        fixperms $user
     done
 }
 
-#Main function, switches options passed to it
+# Main function, switches options passed to it
 case "$1" in
 
-    -h) helptext
-  ;;
-    --help) helptext
-      ;;
+    -h) helptext ;;
+    --help) helptext ;;
     -v) verbose="-v"
 
-  case "$2" in
+    case "$2" in
 
-    -all) all
-           ;;
-    --account) fixperms "$3"
-         ;;
-    -a) fixperms "$3"
+        -all) all ;;
+        --account) fixperms "$3" ;;
+        -a) fixperms "$3" ;;
+        *) 
+            tput bold
+            tput setaf 1
+            echo "Invalid option!"
+            helptext
         ;;
-    *) tput bold
-           tput setaf 1
-       echo "Invalid Option!"
-       helptext
-       ;;
-  esac
-  ;;
-
-    -all) all
+    esac
     ;;
-    --account) fixperms "$2"
-          ;;
-    -a) fixperms "$2"
-  ;;
+
+    -all) all ;;
+    --account) fixperms "$2" ;;
+    -a) fixperms "$2" ;;
     *)
-       tput bold
-       tput setaf 1
-       echo "Invalid Option!"
-       helptext
-       ;;
+        tput bold
+        tput setaf 1
+        echo "Invalid option!"
+        helptext
+    ;;
 esac
